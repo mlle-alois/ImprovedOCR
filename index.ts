afaaf2ee531626420de3df6d6e6a7f59f6d60edf
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2022-02-11 12:18:02 +0100

    [TEST NUMBERS]

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
+import {NumberStrings} from "./number-strings";
+
+for (let i = 0; i < 12; i += 3) {
+    console.log(NumberStrings.number_one.substring(i, i + 3) +
+        NumberStrings.number_two.substring(i, i + 3) +
+        NumberStrings.number_three.substring(i, i + 3) +
+        NumberStrings.number_four.substring(i, i + 3) +
+        NumberStrings.number_five.substring(i, i + 3) +
+        NumberStrings.number_six.substring(i, i + 3) +
+        NumberStrings.number_seven.substring(i, i + 3) +
+        NumberStrings.number_eight.substring(i, i + 3) +
+        NumberStrings.number_nine.substring(i, i + 3))
+}
+
 console.log('Execution')
---
+++ number-strings.ts
@@ -0,0 +1,11 @@
+export class NumberStrings {
+    public static number_one = "     |  |   "
+    public static number_two = " _  _||_    "
+    public static number_three = " _  _| _|   "
+    public static number_four = "   |_|  |   "
+    public static number_five = " _ |_  _|   "
+    public static number_six = " _ |_ |_|   "
+    public static number_seven = " _   |  |   "
+    public static number_eight = " _ |_||_|   "
+    public static number_nine = " _ |_| _|   "
+}
